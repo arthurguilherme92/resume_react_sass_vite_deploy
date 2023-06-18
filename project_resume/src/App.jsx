3fb01fbeeb9735d17df46c18be540fbce18241b3
unknown <arthurguilherme2013@gmail.com>
2023-06-18 13:37:58 +0100

    Finished Social Media Block inside of the Aside

--- project_resume/src/App.jsx
+++ project_resume/src/App.jsx
@@ -1,11 +1,18 @@
-import './sass/app.sass'
+import SideBar from "./components/SideBar";
+import MainPage from "./components/MainPage";
+import './sass/components/app.sass';
 
 function App() {
-  
 
   return (
     <>
-      <h1>Resume Project</h1>
+     <div id="container">
+        <div id="arthur-guilherme">
+          <h1>Arthur Guilherme</h1>
+        </div>
+        <SideBar />
+        <MainPage />
+     </div>
     </>
   )
 }
